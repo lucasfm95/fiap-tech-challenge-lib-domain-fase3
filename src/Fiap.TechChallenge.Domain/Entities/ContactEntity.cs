e2edf4fe6fad876b4efb53a5cecfdc82aeab2e0c
Lucas Martins <martinslf@taginfraestrutura.com.br>
2024-09-11 09:43:56 -0300

    first commit

---
+++ src/Fiap.TechChallenge.Domain/Entities/ContactEntity.cs
@@ -0,0 +1,10 @@
+﻿namespace Fiap.TechChallenge.Domain.Entities;
+
+public class ContactEntity(string name, string email, string phoneNumber, short dddNumber)
+{
+    public long Id { get; set; }
+    public string Name { get; set; } = name;
+    public string Email { get; set; } = email;
+    public string PhoneNumber { get; set; } = phoneNumber;
+    public short DddNumber { get; set; } = dddNumber;
+}
